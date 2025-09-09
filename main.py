ddbe5c098b3220be52b58e3f9240bbda0cabbde8
[Andrew A] <[andrewgtc2005@gmail.com]>
2025-09-09 17:10:41 +0530

    Changes:
    *Added a retrival pipeline.

---
+++ main.py
@@ -0,0 +1,16 @@
+from retrieval.utils import get_relevant_points,get_client
+from retrieval.config import CROSS_ENCODER,COLLECTION_NAME   # loaded at startup
+
+
+def search_vectorstore(query: str,top_k:int,top_rk:int):
+    # your Qdrant query here...
+    client = get_client()
+    if client:
+        points = get_relevant_points(query,client,COLLECTION_NAME,top_k,True,top_rk)
+        return {"results": [p.payload.get("page_content","") for p in points]}
+    else:
+        raise Exception()
+    
+
+if __name__ == "__main__":
+    print(search_vectorstore("Who is the prime minster of india",10,5))
